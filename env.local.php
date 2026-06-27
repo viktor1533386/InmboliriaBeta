5f47339ab6872d308d2daad9e22277f49f045c7e
Victor Quispe Barboza <1533386@utp.edu.pe>
2026-06-27 17:18:53 -0500

    prueba en nube 2

--- env.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Archivo de variables de entorno locales
-// Este archivo está ignorado por Git, así que es seguro poner tus contraseñas aquí.
-
-return [
-    'BREVO_API_KEY' => 'YOUR_API_KEY_HERE',
-    // 'OTRA_VARIABLE' => 'valor',
-];
